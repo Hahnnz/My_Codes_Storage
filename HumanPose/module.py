7a55b76ed61a8be7641ccb8fae40a82a12ab758b
Hahnnz <lphion96@naver.com>
2018-04-19 03:14:51 +0900

    update to python3 and upgraded

--- HumanPose/module.py
+++ HumanPose/module.py
@@ -1,7 +1,7 @@
-from Tkinter import *
-import Tkinter, ttk, cv2, PIL, os, glob, argparse, math
+import tkinter, PIL, os, glob, argparse, math
 import numpy as np
 from PIL import ImageTk, Image
+from tkinter import ttk
 
 Dataset_root="./Image Data Set"
 
@@ -33,7 +33,6 @@ def explore_dir(dir,count=0):
 
 def create_canvas(name):
 	share = 120
-	#coount = 0
 
 	dataset=explore_dir(Dataset_root,0)
 	myshare=[["" for cols in range(share)]for rows in range(2)]
@@ -52,68 +51,85 @@ def create_canvas(name):
 	start = (count * share) - share
 	end = (count * share) - 1
 
-	print start,end
+	print(start,end)
 
 	for i in range(2):
 		for j in range(share):
 			myshare[i][j]=dataset[i][j+start]
 	for i in range(share):
-		testimg=cv2.imread(myshare[1][i])
-		if testimg is None: continue
-		if testimg.shape[0]>max_size[0]: max_size[0]=testimg.shape[0]
-		if testimg.shape[1]>max_size[1]: max_size[1]=testimg.shape[1]
+		img = Image.open(myshare[1][i])
+		[WIDTH, HEIGHT] = img.size
+		if img is None: continue
+		if img.size[0]>max_size[0]: max_size[0]=img.size[0]
+		if img.size[1]>max_size[1]: max_size[1]=img.size[1]
 
 
 
-	root = Tk()
+	root = tkinter.Tk()
 
-	buttonframe=Frame(root)
-	canvasframe=Frame(root)
-	clearbuttonframe=Frame(root)
+	buttonframe=tkinter.Frame(root)
+	canvasframe=tkinter.Frame(root)
+	clearbuttonframe=tkinter.Frame(root)
 
- 	buttonframe.pack(ipadx=0,ipady=0,side="top")
+	buttonframe.pack(ipadx=0, ipady=0, side="top")
 
- 	coor=StringVar(root,value='')
- 	coor_entry=ttk.Entry(root,textvariable=coor,width=20)
- 	coor_entry.pack(side="top")
+	coor=tkinter.StringVar(root,value='')
+	coor_entry=ttk.Entry(root,textvariable=coor,width=100)
+	coor_entry.pack(side="top")
 
- 	full_path=StringVar(root,value='')
- 	filefullpath=ttk.Entry(root,textvariable=full_path,width=80)
- 	filefullpath.pack(side="top")
+	full_path=tkinter.StringVar(root,value='')
+	filefullpath=ttk.Entry(root,textvariable=full_path,width=80)
+	filefullpath.pack(side="top")
 
- 	canvasframe.pack(ipadx=0,ipady=0,side="top")
-	canvas = Canvas(canvasframe,width=max_size[1],heigh=max_size[0])
+	canvasframe.pack(ipadx=0,ipady=0,side="top")
+	canvas = tkinter.Canvas(canvasframe,width=max_size[1],heigh=max_size[0])
 	canvas.pack()
 
 	def imgimg(canvas,imgimg_name):
 		canvas.delete("all")
-		img1=cv2.imread(imgimg_name)
+		resized=False
 
-		HEIGHT,WIDTH = img1.shape[:-1]
-		canvas = Canvas(canvas,width=WIDTH,heigh=HEIGHT)
+		img = Image.open(imgimg_name)
+		OLD_W, OLD_H = img.size
+		NEW_W, NEW_H = 800, 750
+		print(img.size , OLD_W/NEW_W, OLD_H/OLD_H)
+
+		if OLD_W>NEW_W or OLD_H>NEW_H: 
+			img = img.resize((NEW_W, NEW_H), Image.ANTIALIAS)
+			resized=True
+
+		canvas = tkinter.Canvas(canvas, width=NEW_W, heigh=NEW_H)
 		canvas.place(x=0,y=0)
-		img2=ImageTk.PhotoImage(file=imgimg_name)
-		canvas.create_image(0,0,image=img2,anchor="nw")
+
+		img = ImageTk.PhotoImage(img)
+		canvas.create_image(0,0,image=img,anchor="nw")
 		filefullpath.delete(0,"end")
 		filefullpath.insert("end",imgimg_name)
+
 		def down(event):
-		  global x0,y0;
-		  x0, y0 = event.x, event.y
-		  if(x0,y0) == (event.x,event.y):
-		    canvas.create_oval(x0-2,y0-2,event.x+2,event.y+2,outline="red",fill="red",width=2)
+			global x0,y0;
+			x0, y0 = event.x, event.y	  
+
+			if(x0,y0) == (event.x,event.y):
+				canvas.create_oval(x0-2,y0-2,event.x+2,event.y+2,outline="red",fill="red",width=2)
 		def up(event):
-		  global x0, y0
-		  if(x0,y0) == (event.x,event.y):
-		    canvas.create_oval(x0-2,y0-2,event.x+2,event.y+2,outline="red",fill="red",width=2)
-		  coor_entry.delete(0,"end")
-		  coor_entry.insert("end","("+str(x0)+","+str(y0)+")")
-		  print ("("+str(x0)+","+str(y0)+")")
+			global x0, y0
+			if(x0,y0) == (event.x,event.y):
+				canvas.create_oval(x0-2,y0-2,event.x+2,event.y+2,outline="red",fill="red",width=2)
+
+			if resized : 
+				x0*=OLD_W/NEW_W
+				y0*=OLD_H/NEW_H
+			#coor_entry.delete(0,"end")
+			coor_entry.insert("end",str(int(x0))+","+str(int(y0))+",")
+			print ("("+str(int(x0))+","+str(int(y0))+")")
+
 		canvas.bind("<Button-1>",down) 
 		canvas.bind("<ButtonRelease>",up) 
 		root.mainloop()
 	buttonlist=list()
 	for i in range(share):
-		buttonlist.append(Button(buttonframe, text=myshare[0][i],command=lambda i=i: imgimg(canvas,myshare[1][i])))
+		buttonlist.append(tkinter.Button(buttonframe, text=myshare[0][i],command=lambda i=i: imgimg(canvas,myshare[1][i])))
 
 	count_num=0
 	for row in range(int(math.ceil(share/20))):
