36f9ac7badcb9b06d86fef99da7350f451e89bff
Hahnnz <lphion96@naver.com>
2018-06-06 16:23:30 +0900

    added normalization func - VMM, head

--- HumanPose/dataset.py
+++ HumanPose/dataset.py
@@ -1,23 +1,32 @@
 import numpy as np
 import pandas as pd
-import skimage, scipy, glob, os, copy
-from skimage import io, transform
+import scipy, glob, os, copy, cv2
 from scipy.io import loadmat
 from tqdm import tqdm
 
+import tensorflow as tf
+from scripts import dataset
+from tensorflow.contrib.data import Iterator
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework.ops import convert_to_tensor
+
+def one_hot_encoding(labels):
+    return np.eye(np.max(labels) + 1)[labels].reshape(labels.shape[0],np.max(labels) + 1)
+
 class met:
-    def __init__(self, csv_file, re_img_size=(227,227), is_valid=False, Rotate=False, Fliplr=False, Shuffle=False):
-        joints = pd.read_csv(csv_file,header=None).as_matrix()
+    def __init__(self, csv_file, re_img_size=(227,227), is_valid=False, 
+                 Rotate=False, Fliplr=False, Shuffle=False, one_hot=False):
+        joints=pd.read_csv(csv_file,header=None).as_matrix()
         
-        self.re_img_size = re_img_size
-        self.img_path = list(path for path in joints[:,0])
-        self.joint_coors = list(coors for coors in joints[:,1:29])
-        self.joint_is_valid = np.array(list(is_valid for is_valid in joints[:,29:43]))
-        self.labels = np.array(list(labels for labels in joints[:,43]))[:,np.newaxis]
-        self.scores = np.array(list(scores for scores in joints[:,44]))[:,np.newaxis]
+        self.re_img_size=re_img_size
+        self.img_path=list(path for path in joints[:,0])
+        self.joint_coors=np.array(list(coors for coors in joints[:,1:29]))
+        self.joint_is_valid=np.array(list(is_valid for is_valid in joints[:,29:43]))
+        self.labels=np.array(list(labels for labels in joints[:,43]))[:,np.newaxis]
+        self.scores=np.array(list(scores for scores in joints[:,44]))[:,np.newaxis]
         
-        self.img_set = np.zeros([len(self.img_path),re_img_size[0],re_img_size[1],3])
-        self.coor_set = np.array(self.joint_coors).reshape(len(self.joint_coors),14,2)
+        self.img_set=np.zeros([len(self.img_path),re_img_size[0],re_img_size[1],3])
+        self.coor_set=np.array(self.joint_coors).reshape(len(self.joint_coors),14,2)
         
         self.means = self._get_coor_means(csv_file,self.coor_set,max(self.labels)[0]+1)
         
@@ -31,8 +40,8 @@ class met:
         with tqdm(total=len(self.img_path)) as pbar_process:
             pbar_process.set_description("[Processing Images & Coordinates]")
             for i, path in enumerate(self.img_path):
-                img=skimage.io.imread(path)
-                self.img_set[i]=skimage.transform.resize(img,(re_img_size[0],re_img_size[1],3),mode='reflect')
+                img=cv2.imread(path)
+                self.img_set[i]=cv2.resize(img,(re_img_size[0],re_img_size[1]), interpolation=cv2.INTER_CUBIC)
 
                 for j in range(len(self.coor_set[i])):
                     if is_valid and bool(self.joint_is_valid[i][j]): self.coor_set[i][j] = [-1,-1]
@@ -69,10 +78,15 @@ class met:
             self.joint_is_valid = shuffled['valid']
             self.labels = shuffled['labels']
             self.scores = shuffled['scores']
-    
+            
+        if one_hot :
+            self.labels = one_hot_encoding(self.labels)
+            
         self.rel_coor = np.array( list(self._rel_coor(self.coor_set[i]) for i in range(len(self.coor_set))))
-            
-            
+        self.MM_norm_coord = np.array(list(map(self._Vec_MinMaxScaler,self.rel_coor)))
+        self.head_norm_coord = np.array(list(map(self._head_basis,self.rel_coor)))
+    
+    
     def _rotation(self, images, joints, labels, joint_is_valid, scores):
         thetas = np.deg2rad((-30,-20,-10,10,20,30))
         rotated_img = np.zeros([images.shape[0]*len(thetas), images.shape[1],images.shape[2],3])
@@ -95,7 +109,7 @@ class met:
                                              )(coor) for coor in coor_list)
                     
                     rotated_orig= img_rotated.shape[:2]
-                    img_rotated = skimage.transform.resize(img_rotated, (self.re_img_size[0],self.re_img_size[1],3), mode='reflect')
+                    img_rotated = cv2.resize(img_rotated, (self.re_img_size[0],self.re_img_size[1]), interpolation=cv2.INTER_CUBIC)
 
                     rotated_img[(i*len(thetas))+j]=img_rotated
                     rotated_coor[(i*len(thetas))+j]=np.array(rotated_coor_list)*(self.re_img_size[0]/rotated_orig[0])
@@ -140,7 +154,7 @@ class met:
         
         return {'images': shuffled_img,'joints':shuffled_coor,'valid':shuffled_valid,
                 'labels':shuffled_labels,'scores':shuffled_scores}
-    
+
     def _rel_coor(self,coors):
         coors=np.array((coors[:,0],coors[:,1]),dtype=np.float32)
         filt = np.array([[1,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -173,3 +187,42 @@ class met:
                     count+=1
             mean_set[cl]=mean_set[cl]/count
         return mean_set
+            
+    # Normalization functions
+    def _Vec_MinMaxScaler(self, coord):
+        r_coord = coord[:13]**2
+        coord_list = list(r_coord[:,0]+r_coord[:,1])
+
+        minval = coord[coord_list.index(min(coord_list))]
+        maxval = coord[coord_list.index(max(coord_list))]
+        numerator = r_coord - minval
+        denominator = maxval - minval
+        return numerator/ (denominator + 1e-8)
+    
+    def _head_basis(self, coord):
+        return coord[:14]/coord[13]
+    
+class iterator:
+    def __init__(self, csv_file, batch_size, mode, Rotate=False, Fliplr=False, Shuffle=False):
+        if not mode.lower() in {"classification", "regression", "all"}:
+            raise ValueError("mode must be given 'classification', 'regression' or 'all'.")
+        
+        data = met(csv_file,Rotate=Rotate,Fliplr=Fliplr,Shuffle=Shuffle)
+        
+        # True : Classification mode
+        # False : Regression mode
+        self._mode = True if mode.lower()=="classification" else False
+        self.batch_size = batch_size
+        self.num_classes = max(data.labels)[0]+1
+        
+        self.img_set = convert_to_tensor(data.img_set, dtype=dtypes.float64)
+        if self._mode:
+            self.labels = convert_to_tensor(data.labels[:,0], dtype= dtypes.int32)
+        elif not self._mode:
+            self.coor_set = convert_to_tensor(data.coor_set.reshape(len(data.coor_set), -1), dtype = dtypes.float64) 
+
+        data = tf.data.Dataset.from_tensor_slices((self.img_set, tf.one_hot(self.labels, self.num_classes) if self._mode else self.coor_set))
+        data = data.batch(self.batch_size)
+
+        self.iterator = Iterator.from_structure(data.output_types, data.output_shapes)
+        self.init_op = self.iterator.make_initializer(data)
