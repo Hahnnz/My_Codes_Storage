30d049e4a6b828792dbe6e21e220fe52c520ace3
Hahnnz <lphion96@naver.com>
2018-06-20 20:21:55 +0900

    get codes organized

---
+++ deep_learning/caffe/README.md
@@ -0,0 +1,6 @@
+# [Caffe](https://github.com/BVLC/caffe)
+
+Caffe is a deep learning framework made with expression, speed, and modularity in mind.
+It is developed by Berkeley AI Research ([BAIR](http://bair.berkeley.edu))/The Berkeley Vision and Learning Center (BVLC) and community contributors.
+
+Check out the [project site](http://caffe.berkeleyvision.org) for all the details like
--- deep_learning/tensorflow/basic_layers.py
+++ deep_learning/tensorflow/basic_layers.py
@@ -49,24 +49,3 @@ def fc(data, num_in, num_out, name=None, relu=True):
         return tf.nn.relu(output)
     else: 
         return output
-
-def ZeroPadding2D(data,psize,Type="CONSTANT",name=None):
-    data_shape=data.get_shape().as_list()
-    if data_shape[0]==None :
-        raise ValueError("batch_size must be specified.")
-    else:
-        paddings = tf.constant([psize, psize])
-        for batch_size in range(data_shape[0]):
-            padded = tf.pad(data[0,:,:,0],paddings,Type)
-            padded = tf.expand_dims(tf.expand_dims(padded,axis=0),axis=3)
-
-            for channels in range(1,data_shape[3]):
-                padded_2 = tf.pad(data[0,:,:,channels],paddings,Type)
-                padded_2 = tf.expand_dims(tf.expand_dims(padded_2,axis=0),axis=3)
-                padded = tf.concat([padded,padded_2],3)
-
-            if batch_size==0 : 
-                padded_dataset=padded
-            else : 
-                padded_dataset=tf.concat([padded_dataset,padded],0)
-    return padded_dataset
