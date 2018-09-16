4740568482dc725e5803d8026049124a1639580a
hahnnz <lphion96@naver.com>
2018-09-16 21:47:52 +0900

    jupyter_notebook_config.py

---
+++ jupyter/jupyter_notebook_config.py
@@ -0,0 +1,6 @@
+# Configuration file for jupyter-notebook.
+c = get_config()
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.notebook_dir = u'/my_home_dir/'
+c.NotebookApp.open_browser = False
+c.NotebookApp.port = 8888
