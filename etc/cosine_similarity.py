0e19770f32704caf94882e80d5372031b053f202
hahnnz <lphion96@naver.com>
2018-11-05 13:05:40 +0900

    added cosine similarity function

---
+++ etc/cosine_similarity.py
@@ -0,0 +1,9 @@
+def cosine_similarity(v1,v2):
+    sumxx, sumxy, sumyy = 0, 0, 0
+    for i in range(len(v1)):
+        x = v1[i]
+        y = v2[i]
+        sumxx += x*x
+        sumyy += y*y
+        sumxy += x*y
+    return sumxy/(sumxx*sumyy)**0.5
